9db89fdd9bc43cf439b9e43b40d700dd05eff33b
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-02-16 19:10:26 +0000

    implemeted testing

--- test.js
+++ test.js
@@ -1,9 +1,30 @@
+const daysOfMonth = displayDays(2026, 5);
+let string = "";
+for(let i = 1; i <= 7; i++) {
+	if(i < 8) {
+		string = `Day ${dayNamesDictionary(i-1)} in grid = `;
 
-//onsole.log(timeNow.getDay());
+		if(dayNamesDictionary(i-1) == daysOfMonth[0].dayName) {
+			string += daysOfMonth[0].dayName
+		}
+		console.log(string)
+	}
+
+
+}
+//console.log(daysOfMonth[0].dayName);
+
+
+
+
+//console.log(displayDays(2026,5));
+
+
 
 function displayDays(year, month) {
 	const firstDayOfMonth = new Date(year, month - 1, 1);
 	let weekday = firstDayOfMonth.getDay();
+	let result = [];
 
 	for (let i = 1; i <= 35; i++) {
 		if (i !== 1) weekday += 1;
@@ -18,14 +39,13 @@ function displayDays(year, month) {
 			break;
 		}
 
-		console.log(`${i} = ${dayNamesDictionary(weekday)}`);
+		result.push({
+			dayNumber: i,
+			dayName: dayNamesDictionary(weekday)
+		})
 	}
+	return result;
 }
-
-
-
-
-displayDays(2026,5);
 
 function dayNamesDictionary(DayNumber) {
 	dayNames = {
