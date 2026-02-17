aca39b8f031dc22e48752d0945c6e36008031aa1
Rahma Berhan <122676773+RahmaB1@users.noreply.github.com>
2026-02-17 15:10:28 +0000

    refactor calendar layout and improve month/year selection functionality

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-import { GetDaysOfMonth, dayNamesDictionary} from "./logic.js";
-
+import { GetDaysOfMonth, dayNamesDictionary } from "./logic.js";
 
 const currentDate = new Date(); //Tue Feb 17 2026 01:05:10 GMT+0000 (Greenwich Mean Time)
 
@@ -19,92 +18,97 @@ const monthNames = [
   "September",
   "October",
   "November",
-  "December"
+  "December",
 ];
 let monthAndYearState = {
   month: currentMonth,
-  year: currentYear
+  year: currentYear,
 };
 
 const monthTitle = document.getElementById("monthTitle");
 const calendarGrid = document.getElementById("calendarGrid");
 
-monthTitle.textContent= `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`; 
+monthTitle.textContent = `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`;
 
 let daysOfMonth = GetDaysOfMonth(currentYear, currentMonth + 1); // this is for first render to display current month
 
 function displayCalendarDays(daysOfMonth) {
-	let string = "";
-	let displayedDay = "";
+  let string = "";
+  let displayedDay = "";
 
-	// for disabled squares in the beginning of calendar table
-	for(let x = 1; x<= daysOfMonth[0].weekDayNumber; x++) {
-		string += `<div class="empty">x</div>`;
-	}
+  // for disabled squares in the beginning of calendar table
+  for (let x = 1; x <= daysOfMonth[0].weekDayNumber; x++) {
+    string += `<div class="empty">x</div>`;
+  }
 
-	//start showing days
-	for(let i = 1; i <= daysOfMonth.length; i++) {
-		displayedDay = i;
+  //start showing days
+  for (let i = 1; i <= daysOfMonth.length; i++) {
+    displayedDay = i;
 
-		string += `<div class="day">${displayedDay}</div> `;
-	}
+    string += `<div class="day">${displayedDay}</div> `;
+  }
 
-	// for disabled squares in the beginning of calendar table
-	for(let x = 1; x<= 6 - daysOfMonth[daysOfMonth.length - 1].weekDayNumber; x++) {
-		string += `<div class="empty">x</div>`;
-	}
-	return string;
+  // for disabled squares in the beginning of calendar table
+  for (
+    let x = 1;
+    x <= 6 - daysOfMonth[daysOfMonth.length - 1].weekDayNumber;
+    x++
+  ) {
+    string += `<div class="empty">x</div>`;
+  }
+  return string;
 }
 calendarGrid.innerHTML = displayCalendarDays(daysOfMonth);
 
-
-
-//event listener for the select 
+//event listener for the select
 
 const monthSelect = document.getElementById("monthSelect");
-//create the months options 
-for(let i = 0 ; i < 12 ; i++){
-const option = document.createElement("option");
-option.value = `${i}`;
-option.textContent = monthNames[i];
-if (i === currentMonth) {
-  option.selected = true; // Set the current month as selected
-}	
-monthSelect.appendChild(option);
+//create the months options
+for (let i = 0; i < 12; i++) {
+  const option = document.createElement("option");
+  option.value = `${i}`;
+  option.textContent = monthNames[i];
+  if (i === currentMonth) {
+    option.selected = true; // Set the current month as selected
+  }
+  monthSelect.appendChild(option);
 }
 
-monthSelect.addEventListener("change",(e)=> {
-monthAndYearState.month = Number(e.target.value); // string to number 
+monthSelect.addEventListener("change", (e) => {
+  monthAndYearState.month = Number(e.target.value); // string to number
   console.log(monthAndYearState.month);
-  daysOfMonth = GetDaysOfMonth(monthAndYearState.year, monthAndYearState.month +1);
-//   monthTitle.textContent= `${monthNames[monthAndYearState.month]} `;
- monthTitle.textContent= `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`; 
+  daysOfMonth = GetDaysOfMonth(
+    monthAndYearState.year,
+    monthAndYearState.month + 1,
+  );
+  //   monthTitle.textContent= `${monthNames[monthAndYearState.month]} `;
+  monthTitle.textContent = `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`;
 
   calendarGrid.innerHTML = displayCalendarDays(daysOfMonth);
-
-} );
+});
 
 const yearSelect = document.getElementById("yearSelect");
 
-for(let i = currentYear - 26 ; i <= currentYear +26 ; i++){
-const option = document.createElement("option");
-option.value = `${i}`;
-option.textContent = i;
-if (i === currentYear) {
-  option.selected = true; // Set the current year as selected
-}
-// option.textContent += i+1;
-yearSelect.appendChild(option);
+for (let i = currentYear - 26; i <= currentYear + 26; i++) {
+  const option = document.createElement("option");
+  option.value = `${i}`;
+  option.textContent = i;
+  if (i === currentYear) {
+    option.selected = true; // Set the current year as selected
+  }
+  // option.textContent += i+1;
+  yearSelect.appendChild(option);
 }
 
-yearSelect.addEventListener("change",(e)=> {
-monthAndYearState.year = Number(e.target.value);
+yearSelect.addEventListener("change", (e) => {
+  monthAndYearState.year = Number(e.target.value);
   console.log(monthAndYearState.year);
-  daysOfMonth = GetDaysOfMonth(monthAndYearState.year, monthAndYearState.month +1);
- monthTitle.textContent= `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`; 
+  daysOfMonth = GetDaysOfMonth(
+    monthAndYearState.year,
+    monthAndYearState.month + 1,
+  );
+  monthTitle.textContent = `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`;
   calendarGrid.innerHTML = displayCalendarDays(daysOfMonth);
-
-
 });
 
 //event listeners for the next/previous buttons
@@ -117,8 +121,11 @@ prevBtn.addEventListener("click", () => {
     monthAndYearState.month = 11;
     monthAndYearState.year--;
   }
-  daysOfMonth = GetDaysOfMonth(monthAndYearState.year, monthAndYearState.month +1);
-  monthTitle.textContent= `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`; 
+  daysOfMonth = GetDaysOfMonth(
+    monthAndYearState.year,
+    monthAndYearState.month + 1,
+  );
+  monthTitle.textContent = `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`;
   calendarGrid.innerHTML = displayCalendarDays(daysOfMonth);
 });
 
@@ -128,9 +135,12 @@ nextBtn.addEventListener("click", () => {
     monthAndYearState.month = 0;
     monthAndYearState.year++;
   }
-  daysOfMonth = GetDaysOfMonth(monthAndYearState.year, monthAndYearState.month +1);
-  monthTitle.textContent= `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`; 
+  daysOfMonth = GetDaysOfMonth(
+    monthAndYearState.year,
+    monthAndYearState.month + 1,
+  );
+  monthTitle.textContent = `${monthNames[monthAndYearState.month]} ${monthAndYearState.year}`;
   calendarGrid.innerHTML = displayCalendarDays(daysOfMonth);
 });
 
-//want to change the selected month and year in the select too 
+//want to change the selected month and year in the select too
