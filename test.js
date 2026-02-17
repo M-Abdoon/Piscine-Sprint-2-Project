87428fc71e238dcf40089111b1520ef16c899e0e
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-02-17 21:27:17 +0000

    completed inplementation of getSpecialDays()

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import daysData from "./days.json" with { type: "json" };
 
 const calendarGrid = document.getElementById("calendarGrid");
 
-const daysOfMonthData = GetDaysOfMonth(2020, 2);
+const daysOfMonthData = GetDaysOfMonth(2020, 10);
 
 //console.log(daysOfMonthData);
 
@@ -15,12 +15,26 @@ function getSpecialDays(daysOfMonthData, month) { // month is optional parameter
 
 	if(monthsFiltered != 0) { // means we have special days in this month
 		
+		let specialDayIs = "";
 		monthsFiltered.forEach(specialDay => { // assume dayName is Tuesday
+			const nameOfDay = specialDay.dayName;
+			const occurrence = specialDay.occurrence;
 
-			// number of how many times this day is repeated in that month
-			const countsOfSelectedDayNames = daysOfMonthData.filter(day => day.dayName == specialDay.dayName).length;
+			// info of day names repeated in that month
+			const countsOfSelectedDayNames = daysOfMonthData.filter(day => day.dayName == nameOfDay);
 
-			console.log(countsOfSelectedDayNames);
+			if(occurrence === "first")
+				specialDayIs = countsOfSelectedDayNames[0];
+			else if (occurrence === "second")
+				specialDayIs = countsOfSelectedDayNames[1];
+			else if (occurrence === "third")
+				specialDayIs = countsOfSelectedDayNames[2];
+			else if (occurrence === "forth")
+				specialDayIs = countsOfSelectedDayNames[3];
+			else if (occurrence === "last")
+				specialDayIs = countsOfSelectedDayNames[4];
+
+			console.log(specialDayIs);
 		})
 		
 	}
