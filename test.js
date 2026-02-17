960de123b9794de396f84cc67d3702237e529e7a
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-02-17 21:08:16 +0000

    implementing getSpecialDays() - getting number of day names in current month

--- test.js
+++ test.js
@@ -8,9 +8,25 @@ const daysOfMonthData = GetDaysOfMonth(2020, 2);
 
 //console.log(daysOfMonthData);
 
-function getSpecialDays(daysOfMonthData) {
+function getSpecialDays(daysOfMonthData, month) { // month is optional parameter
+	// month now is - for example - october
+	month = "October";
+	const monthsFiltered = daysData.filter(specialDay => specialDay.monthName == month);
 
-	return daysData;
+	if(monthsFiltered != 0) { // means we have special days in this month
+		
+		monthsFiltered.forEach(specialDay => { // assume dayName is Tuesday
+
+			// number of how many times this day is repeated in that month
+			const countsOfSelectedDayNames = daysOfMonthData.filter(day => day.dayName == specialDay.dayName).length;
+
+			console.log(countsOfSelectedDayNames);
+		})
+		
+	}
+
+	console.log(daysOfMonthData);
+	console.log(daysData);
 }
 
 console.log(getSpecialDays(daysOfMonthData));
