c27c674ada4020dbf6ecb93644f9d51f9a48bc0d
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-02-18 12:49:47 +0000

    basic implemetnation of generateICS

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,7 +1,42 @@
-// // This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
-// // It can be run with `node`.
+import { dayNamesDictionary, monthNamesDictionary } from "./logic.js";
+import daysData from "./days.json" with { type: "json" };
 
-// import { getGreeting } from "./common.mjs";
-// import daysData from "./days.json" with { type: "json" };
 
-// console.log(`{getGreeting()} - there are ${daysData.length} known days`);
+function generateIcs() {
+
+	let string = `
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//DaysCalendar-Rahma-Abdoon//EN
+`;
+	daysData.forEach( day => {
+		const name = "first Friday Feb";
+		const dayName = day.dayName;
+		const occurrence = day.occurrence;
+
+
+		const PRule = {
+			byMonth: day.monthName,
+			byDate: `${occurrence}${dayName}`,
+			summary: name
+	
+		}
+		string += `
+	BEGIN:VEVENT
+	UID:first-friday-feb@ourDaysCalendar
+	DTSTAMP:20260218T120000Z
+	DTSTART:00010101T090000Z
+	RRULE:FREQ=YEARLY;BYMONTH=${PRule.byMonth};BYDAY=${PRule.byDate};UNTIL=30001231T235959Z
+	SUMMARY:${PRule.summary}
+	END:VEVENT
+	`;
+	})
+
+string += `
+END:VCALENDAR
+`;
+	console.log(string);
+//	return string;
+}
+
+console.log(generateIcs());
--- logic.js
+++ logic.js
@@ -46,20 +46,20 @@ export function dayNamesDictionary(DayNumber) {
 }
 
 export function monthNamesDictionary(monthNumber) {
-	const monthNames = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December"
-	];
+	const monthNames = {
+		0: "January",
+		1: "February",
+		2: "March",
+		3: "April",
+		4: "May",
+		5: "June",
+		6: "July",
+		7: "August",
+		8: "September",
+		9: "October",
+		10: "November",
+		11: "December"
+	};
 	return monthNames[monthNumber];
 }
 
