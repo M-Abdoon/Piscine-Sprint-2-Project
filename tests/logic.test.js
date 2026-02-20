ed14ac1e40f42184e666f57c2d92ec8d4e6ed865
Rahma Berhan <122676773+RahmaB1@users.noreply.github.com>
2026-02-20 02:16:26 +0000

    adding tests for two functions

--- tests/logic.test.js
+++ tests/logic.test.js
@@ -2,78 +2,59 @@ import assert from "node:assert";
 import test from "node:test";
 import { GetDaysOfMonth, getSpecialDays } from "../logic.js";
 
-// test("Greeting is correct", () => {
-//   assert.equal(1, 1);
-// });
+//------------------ testing GetDaysOfMonth() ------------------
 
-//test getSpecialDays
+test("GetDaysOfMonth() returns the correct number of days for February 2026", () => {
+  assert.equal(GetDaysOfMonth(2026, 2).length, 28);
+});
 
-test("getSpecialDays returns correct special day", () => {
-  const mockMonthData = [
-    { dayNumber: 1, dayName: "Thursday", weekDayNumber: 4 },
-    { dayNumber: 2, dayName: "Friday", weekDayNumber: 5 },
-    { dayNumber: 3, dayName: "Saturday", weekDayNumber: 6 },
-    { dayNumber: 4, dayName: "Sunday", weekDayNumber: 0 },
-    { dayNumber: 5, dayName: "Monday", weekDayNumber: 1 },
-    { dayNumber: 6, dayName: "Tuesday", weekDayNumber: 2 },
-    { dayNumber: 7, dayName: "Wednesday", weekDayNumber: 3 },
-    { dayNumber: 8, dayName: "Thursday", weekDayNumber: 4 },
-    { dayNumber: 9, dayName: "Friday", weekDayNumber: 5 },
-    { dayNumber: 10, dayName: "Saturday", weekDayNumber: 6 },
-    { dayNumber: 11, dayName: "Sunday", weekDayNumber: 0 },
-    { dayNumber: 12, dayName: "Monday", weekDayNumber: 1 },
-    {
-      dayNumber: 13,
-      dayName: "Tuesday",
-      weekDayNumber: 2,
-      isSpecialDay: {
-        name: "Ada Lovelace Day",
-        occurrence: "second",
-        descriptionURL:
-          "https://codeyourfuture.github.io/The-Piscine/days/ada.txt",
-      },
-    },
-    { dayNumber: 14, dayName: "Wednesday", weekDayNumber: 3 },
-    { dayNumber: 15, dayName: "Thursday", weekDayNumber: 4 },
-    { dayNumber: 16, dayName: "Friday", weekDayNumber: 5 },
-    { dayNumber: 17, dayName: "Saturday", weekDayNumber: 6 },
-    { dayNumber: 18, dayName: "Sunday", weekDayNumber: 0 },
-    { dayNumber: 19, dayName: "Monday", weekDayNumber: 1 },
-    { dayNumber: 20, dayName: "Tuesday", weekDayNumber: 2 },
-    { dayNumber: 21, dayName: "Wednesday", weekDayNumber: 3 },
-    { dayNumber: 22, dayName: "Thursday", weekDayNumber: 4 },
-    { dayNumber: 23, dayName: "Friday", weekDayNumber: 5 },
-    { dayNumber: 24, dayName: "Saturday", weekDayNumber: 6 },
-    { dayNumber: 25, dayName: "Sunday", weekDayNumber: 0 },
-    { dayNumber: 26, dayName: "Monday", weekDayNumber: 1 },
-    { dayNumber: 27, dayName: "Tuesday", weekDayNumber: 2 },
-    { dayNumber: 28, dayName: "Wednesday", weekDayNumber: 3 },
-    { dayNumber: 29, dayName: "Thursday", weekDayNumber: 4 },
-    {
-      dayNumber: 30,
-      dayName: "Friday",
-      weekDayNumber: 5,
-      isSpecialDay: {
-        name: "World Lemur Day",
-        occurrence: "last",
-        descriptionURL:
-          "https://codeyourfuture.github.io/The-Piscine/days/lemurs.txt",
-      },
-    },
-    { dayNumber: 31, dayName: "Saturday", weekDayNumber: 6 },
-  ];
-  const specialDays = [
-    {
-      name: "Ada Lovelace Day",
-      monthName: "October",
-      dayName: "Tuesday",
-      occurrence: "second",
-      descriptionURL:
-        "https://codeyourfuture.github.io/The-Piscine/days/ada.txt",
-    },
+test("GetDaysOfMonth() returns the correct number of days for February 2024 (leap year)", () => {
+  assert.equal(GetDaysOfMonth(2024, 2).length, 29);
+});
+
+test("GetDaysOfMonth() returns the correct data type , expecting array", () => {
+  assert.equal(typeof GetDaysOfMonth(2024, 2), "object");
+});
+
+//------------------ testing getSpecialDays() ------------------
+
+test("getSpecialDays() returns the correct event names for October 2025", () => {
+  const daysofOctober2025 = [
+    { dayNumber: 1, dayName: "Wednesday", weekDayNumber: 3 },
+    { dayNumber: 2, dayName: "Thursday", weekDayNumber: 4 },
+    { dayNumber: 3, dayName: "Friday", weekDayNumber: 5 },
+    { dayNumber: 4, dayName: "Saturday", weekDayNumber: 6 },
+    { dayNumber: 5, dayName: "Sunday", weekDayNumber: 0 },
+    { dayNumber: 6, dayName: "Monday", weekDayNumber: 1 },
+    { dayNumber: 7, dayName: "Tuesday", weekDayNumber: 2 },
+    { dayNumber: 8, dayName: "Wednesday", weekDayNumber: 3 },
+    { dayNumber: 9, dayName: "Thursday", weekDayNumber: 4 },
+    { dayNumber: 10, dayName: "Friday", weekDayNumber: 5 },
+    { dayNumber: 11, dayName: "Saturday", weekDayNumber: 6 },
+    { dayNumber: 12, dayName: "Sunday", weekDayNumber: 0 },
+    { dayNumber: 13, dayName: "Monday", weekDayNumber: 1 },
+    { dayNumber: 14, dayName: "Tuesday", weekDayNumber: 2 },
+    { dayNumber: 15, dayName: "Wednesday", weekDayNumber: 3 },
+    { dayNumber: 16, dayName: "Thursday", weekDayNumber: 4 },
+    { dayNumber: 17, dayName: "Friday", weekDayNumber: 5 },
+    { dayNumber: 18, dayName: "Saturday", weekDayNumber: 6 },
+    { dayNumber: 19, dayName: "Sunday", weekDayNumber: 0 },
+    { dayNumber: 20, dayName: "Monday", weekDayNumber: 1 },
+    { dayNumber: 21, dayName: "Tuesday", weekDayNumber: 2 },
+    { dayNumber: 22, dayName: "Wednesday", weekDayNumber: 3 },
+    { dayNumber: 23, dayName: "Thursday", weekDayNumber: 4 },
+    { dayNumber: 24, dayName: "Friday", weekDayNumber: 5 },
+    { dayNumber: 25, dayName: "Saturday", weekDayNumber: 6 },
+    { dayNumber: 26, dayName: "Sunday", weekDayNumber: 0 },
+    { dayNumber: 27, dayName: "Monday", weekDayNumber: 1 },
+    { dayNumber: 28, dayName: "Tuesday", weekDayNumber: 2 },
+    { dayNumber: 29, dayName: "Wednesday", weekDayNumber: 3 },
+    { dayNumber: 30, dayName: "Thursday", weekDayNumber: 4 },
+    { dayNumber: 31, dayName: "Friday", weekDayNumber: 5 },
   ];
 
-  result = getSpecialDays(mockMonthData, "October");
+  const result = getSpecialDays(daysofOctober2025, "October");
 
-  //   assert.deepEqual(result,
+  assert.equal(result[13].isSpecialDay.name, "Ada Lovelace Day");
+  assert.equal(result[30].isSpecialDay.name, "World Lemur Day");
 });
